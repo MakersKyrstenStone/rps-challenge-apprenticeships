b3996d4793f9ffbfae3cd3618d3d26d14967de75
Kyrsten <Kyrsten>
2022-02-04 11:12:49 +0000

    first test trial

--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -3,4 +3,11 @@ feature 'test page' do
     visit '/test'
     expect(page).to have_content('test page')
   end
+
+  feature 'startpage' do
+    scenario 'show the home page with welcome' do
+      visit '/'
+      expect(page).to have_content('Welcome')
+    end
+  end
 end
